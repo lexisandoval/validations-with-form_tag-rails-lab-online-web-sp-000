3ac051ec92acbc04b7758ea3bf5b59afe7863edd
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-21 17:10:43 -0500

    commit tree 27d550839b50a7485bfffb0ab0b56ffea543d9ff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
+  
   def show
     @post = Post.find(params[:id])
   end
+
+  def new 
+  end 
+
+  def create 
+  end 
 
   def edit
     @post = Post.find(params[:id])
@@ -9,10 +16,11 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else 
+      render :edit 
+    end 
   end
 
   private
@@ -20,4 +28,5 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:title, :category, :content)
   end
+
 end
